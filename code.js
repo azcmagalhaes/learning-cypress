91e0ff3a56511ab5ca238caf979dcf4f87884bdd
Adriene Zilda Correa Magalhaes <vntadco@azildacorreamaga.local>
2021-02-22 10:39:32 -0300

    New feature to add images

--- code.js
+++ code.js
@@ -3,4 +3,15 @@ function myFunction() {
     var name = document.getElementById("name").value;
     var text = name + " is " + selected;
     document.getElementById("demo").innerHTML=text;
+    document.getElementById("output").style.display = 'block';
+}
+
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+function functionClear() {
+    document.getElementById("demo").innerHTML=null;
+    document.getElementById("output").style.display = 'none';   
 }
